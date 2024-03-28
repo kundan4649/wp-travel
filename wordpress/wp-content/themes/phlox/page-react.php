42b035d6b6a98536a0f3e10e8cbe61a565ea5498
kundan singh <k.singh4649@gmail.com>
2024-03-28 14:36:27 +0530

    First Check In

---
+++ wordpress/wp-content/themes/phlox/page-react.php
@@ -0,0 +1,12 @@
+
+<?php
+// Template Name: React page
+get_header();
+?>
+
+<div id="root" class="root">kundan1</div>
+<script src="<?php echo get_stylesheet_directory_uri() . '/build/bundle.js'; ?>"></script>
+
+<?php get_footer(); ?>
+
+
---
+++ wordpress/wp-content/themes/twentytwentyfour/page-react.php
@@ -0,0 +1,12 @@
+
+<?php
+// Template Name: React page
+
+?>
+
+<div id="root" class="root">kundan1</div>
+<script src="<?php echo get_stylesheet_directory_uri() . '/build/bundle.js'; ?>"></script>
+
+<?php get_footer(); ?>  
+
+
